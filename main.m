26930c7420d1a948fe1485f7c1a82b57e137334d
MDong <57316584+mxd888@users.noreply.github.com>
2024-01-04 10:17:52 +0800

    Delete %%%

--- main.m
+++ main.m
@@ -1,20 +1,2 @@
-%% 基于无人机交付和动态飞行续航的车辆路径问题多目标优化模型 MATLAB代码复现
-% 
-% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
-% All rights reserved. Please read the "license.txt" for license terms.
-% 
-% 1、卡车搭载无人机实现物流交付
-% 2、巴拉巴拉
-% 3、巴拉巴拉
-% addpath(genpath('./'))
-% 
-% Project Code: YPAP108
-% Project Title: 基于无人机交付和动态飞行续航的车辆路径问题多目标优化模型
-% Publisher: Yarpiz (www.yarpiz.com)
-% 
-% Developer: MDong（广西民族大学混杂实验室）
-% 
-% Contact Info: 1191998028@qq.com, mixiaodong930@163.com
-%
 
 VRPD;
